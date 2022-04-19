47b539f959ec800717cdf71a37a41b06500df735
Raissa C. Correia <raisssa.correia@gmail.com>
2022-04-19 12:49:50 -0300

    starting first tutorial

---
+++ src/print.rs
@@ -0,0 +1,7 @@
+pub fn run() {
+    println!("Hello, world from print.rs!");
+
+    println!("{0} is from {1}, {0}", "Brad", "Mass");
+
+    println!("{name} likes to play {activity}", name="John", activity="Baseball");
+}
